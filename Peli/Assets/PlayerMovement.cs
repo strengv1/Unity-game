992daf7ec6a254559a2876ece040336ac45b5c87
strengv1 <56875243+strengv1@users.noreply.github.com>
2019-10-22 21:18:35 +0300

    Movement a bit smoother now

--- Peli/Assets/PlayerMovement.cs
+++ Peli/Assets/PlayerMovement.cs
@@ -6,25 +6,26 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
 
-    public float speed = 500f;
+    public float movementSpeed = 1f;
+    
     // Update is called once per frame
     void FixedUpdate()
     {
         if ( Input.GetKey("w") )
-        {            
-            rb.AddForce(0, 0, speed * Time.deltaTime);
+        {
+            transform.Translate(0f, 0f, movementSpeed * Time.deltaTime);
+        }
+        if ( Input.GetKey("a") )
+        {
+            transform.Translate(-movementSpeed * Time.deltaTime, 0f, 0f);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(0f, 0f, -movementSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(movementSpeed * Time.deltaTime, 0f, 0f);
         } 
-        else if ( Input.GetKey("a") )
-        {            
-            rb.AddForce(-speed * Time.deltaTime, 0, 0);
-        }
-        else if (Input.GetKey("s"))
-        {
-            rb.AddForce(0, 0, -speed * Time.deltaTime);
-        }
-        else if (Input.GetKey("d"))
-        {
-            rb.AddForce(speed * Time.deltaTime, 0, 0);
-        }
     }
 }
