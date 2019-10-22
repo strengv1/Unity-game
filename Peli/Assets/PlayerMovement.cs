5d47372e806775646ce1af55d6a88685d3a5c783
strengv1 <56875243+strengv1@users.noreply.github.com>
2019-10-22 19:38:22 +0300

    Now the block moves
    Next I gotta smooth out the movement and make the camera follow the player

---
+++ Peli/Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float speed = 500f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if ( Input.GetKey("w") )
+        {            
+            rb.AddForce(0, 0, speed * Time.deltaTime);
+        } 
+        else if ( Input.GetKey("a") )
+        {            
+            rb.AddForce(-speed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -speed * Time.deltaTime);
+        }
+        else if (Input.GetKey("d"))
+        {
+            rb.AddForce(speed * Time.deltaTime, 0, 0);
+        }
+    }
+}
