7a600fb1ea5e2902b860df15c5cdc8016748ff1a
strengv1 <56875243+strengv1@users.noreply.github.com>
2019-10-24 01:26:05 +0300

    Camera now follows fox
    How the heck do I make him jump?

--- Peli/Assets/FollowPlayer.cs
+++ Peli/Assets/FollowPlayer.cs
@@ -2,17 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class animationScript : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public Transform player;
+    public Vector3 offset;
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = player.position + offset;
     }
 }
--- Peli/Assets/PlayerMovement.cs
+++ Peli/Assets/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class playerMovement : MonoBehaviour
 {
     public Animator foxAnim;
     public LayerMask movementMask;
+    public Rigidbody rb;
+    public float jumpPower;
     Camera cam;
     NavMeshAgent agent;
 
@@ -17,14 +19,18 @@ public class playerMovement : MonoBehaviour
         foxAnim = GetComponentInChildren<Animator>();
         cam = Camera.main;
         agent = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        // Jump!
+        if ( Input.GetKeyDown( KeyCode.Space )) {
+            jump();
+        }
+        // Read the location of the mouse when clicking on "ground" and move the foxy there.
         if ( Input.GetMouseButtonDown(0) )
         {               
-            //foxAnim.SetBool("walking", true);
-
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
@@ -33,18 +39,23 @@ public class playerMovement : MonoBehaviour
                 MoveToPoint(hit.point);
             }
         }
-        
-        if(agent.hasPath) {
+   
+        // Change the "walking" parameter, which controls what animation we play.
+        if( agent.hasPath ) {
             foxAnim.SetBool("walking", true);
         } else { 
             foxAnim.SetBool("walking", false);
         }
-
     }
 
     public void MoveToPoint(Vector3 point)
     {
         agent.SetDestination(point);
+    }
 
+    public void jump()
+    {
+        Debug.Log("jump");
+        rb.AddForce(0f, jumpPower, 0f);
     }
 }
