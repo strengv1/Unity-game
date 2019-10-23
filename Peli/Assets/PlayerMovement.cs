8e0e800a3c56efa4eafcff1e5da33f88cc22a7ee
strengv1 <56875243+strengv1@users.noreply.github.com>
2019-10-23 22:06:34 +0300

    The fox walks!
    Created a fox model and animated it. The walking animation is a bit slow, got to find a way to make it more responsive..

--- Peli/Assets/PlayerMovement.cs
+++ Peli/Assets/PlayerMovement.cs
@@ -1,31 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+
+[RequireComponent(typeof(NavMeshAgent))]
 public class playerMovement : MonoBehaviour
 {
+    public Animator foxAnim;
     public LayerMask movementMask;
     Camera cam;
+    NavMeshAgent agent;
 
     void Start()
     {
+        foxAnim = GetComponentInChildren<Animator>();
         cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
     }
 
     void Update()
     {
         if ( Input.GetMouseButtonDown(0) )
-        {
+        {               
+            foxAnim.SetBool("walking", true);
+
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
             if( Physics.Raycast(ray, out hit, 100, movementMask)) // ray, "out hit", range?, mask
             {
-                Debug.Log("We hit" + hit.collider.name + " " + hit.point);
-                // Move our player to what we hit
-
-                //Stop focusing 
+                MoveToPoint(hit.point);
             }
         }
+        
+        if(!agent.hasPath)
+        {
+            foxAnim.SetBool("walking", false);
+        }
+
+    }
+
+    public void MoveToPoint(Vector3 point)
+    {
+        agent.SetDestination(point);
+
     }
 }
